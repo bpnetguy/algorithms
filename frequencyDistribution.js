2044d0a17d00de9575cc8b7eabe40a09d91a1f2a
Binh Phan <binh.phan@gmail.com>
2013-06-14 22:49:23 -0700

    add permuation and combination and freq dis

---
+++ frequencyDistribution.js
@@ -0,0 +1,29 @@
+var testScores = [1,3,4,4,5,6,7,4,7,7];
+
+
+var createFrequencyMatrix = function(a) {
+   var hashTable = {};
+   var len = a.length;
+   while(len--) {
+      var val = a[len];
+      if(hashTable[val]) { 
+         hashTable[val]++;
+      } else {
+         hashTable[val]=1;
+      }
+   }
+   return hashTable;
+}
+
+var frequencyTable = createFrequencyMatrix(testScores);
+
+var scoreKeys = Object.keys(frequencyTable).sort();
+
+var len = scoreKeys.length; 
+while(len--) {
+   var score = scoreKeys[len]; 
+   var frequency = frequencyTable[score];
+   console.log("Score: " + score + " F: " + frequency);
+}
+
+
